b0977199b92e12b567f54e003fa0afdc93684a5b
Dera Uchenwoke <uchidera1@gmail.com>
2022-08-06 07:54:25 +0100

    got description and link of products

--- app.js
+++ app.js
@@ -8,8 +8,25 @@ const axios = require('axios');
 async function checkGet() {
     try{
         axios.get('https://www.asos.com/women/ctas/generic-promos/promotion-4/cat/?cid=28025&ctaref=globalbanner|ww|50offsummerstock').then(urlResponse => {
-            console.log(urlResponse.data); 
+            // Passes in the HTML document for the website.
+            const $ = cheerio.load(urlResponse.data); 
 
+            // Uses the selector 'article' to search the scope within in.
+            // This gets the link to each item.
+            $('article.KCXt8Eb').each((i, element) => {
+                const link = $(element)
+                .find('a.B36cezB')
+                .attr('href'); 
+
+              // Gets description of item.
+                const description = $(element)
+                .find('div.zrNElLX')
+                .text(); 
+
+                console.log(description); 
+                console.log(link);
+                console.log('\n');
+            })
         });
     } catch (err) {
         
